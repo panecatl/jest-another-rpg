6f679f81d655d2dd864c990014599120bafe64e1
panecatl <ypanecatl24@gmail.com>
2022-07-05 16:26:18 -0500

    finished test for player

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -50,6 +50,14 @@ test('checks if player is alive or not', () => {
     expect(player.isAlive()).toBeFalsy();
 });
 
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
 test("subtracts from player's health", () => {
     const player = new Player('Dave');
     const oldHealth = player.health;
@@ -61,6 +69,16 @@ test("subtracts from player's health", () => {
     player.reduceHealth(99999);
 
     expect(player.health).toBe(0);
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
 });
 
 test('adds a potion to the inventory', () => {
